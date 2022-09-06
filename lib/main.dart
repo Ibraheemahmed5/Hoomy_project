348187fb56348ba565d045f86dadd913ffb3ea48
Ibraheemahmed5 <92911409+Ibraheemahmed5@users.noreply.github.com>
2022-09-06 22:16:57 +0300

    commit message

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hoomy_project1/test.dart';
+
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Color(0xFFEFEFEF),
+    statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
+  ));
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: test(),
+    );
+  }
+}
