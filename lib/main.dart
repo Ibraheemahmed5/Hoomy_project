6acf3235e87cafd0b93b9d6e9b5568b6b823f930
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2022-09-08 00:36:42 +0300

    Sura's update 8/9

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hoomy_project1/test.dart';
+import 'splash/splash_screen.dart';
+import 'landing/landing_page.dart';
 
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.black87,
+    statusBarColor: Color(0xff45B9EE),
     statusBarIconBrightness: Brightness.dark,
     statusBarBrightness: Brightness.light,
-  ));
+   ));
   runApp(
     MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: test(),
+      home: Splash(),
     );
   }
 }
